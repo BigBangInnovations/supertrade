5675ff8861b90aac8c060ad3b37f57c1f7e11aec
kaushikbigbang <kaushik.solanki@bigbanginnovations.in>
2019-08-14 12:23:49 +0530

    env. change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
 	production: false,
 	isMockEnabled: false, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-	superSALESApi: 'http://supersales.co:8080/salesprotest/',
-	commonServiceApi: 'http://bigbangcommonservice.info:8080/commonServices3/',
-	apiEndpoint: 'http://34.219.152.38/superloyaltestapi/',
+	superSALESApi: 'http://supersales.co:8080/salesprodev/',
+	commonServiceApi: 'http://bigbangcommonservice.info:8080/commonServices10/',
+	apiEndpoint: 'http://34.219.152.38/superloyaldevapi/',
 	// apiEndpoint: 'http://localhost/superloyalapi/superloyalapi/',
 	encKey: '$bigang_Secure@production',
 	localStorageKey:'_K'
